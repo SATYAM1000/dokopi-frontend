8d79be607093817cc523d4d1300c6971293f9956
satyamx <satyamsingh748846@gmail.com>
2024-05-09 15:01:07 +0530

    location modal done

--- src/components/user/LocationAccess.jsx
+++ src/components/user/LocationAccess.jsx
@@ -1,7 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 
 import {
   Dialog,
@@ -15,15 +14,27 @@ import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import { getUserCoordinates } from "@/lib/getlocation";
 
-
 import { ChevronDown } from "lucide-react";
-
 
 const LocationAccess = ({ apiKey }) => {
   const [location, setLocation] = useState(null);
   const [userAddress, setUserAddress] = useState(null);
   const [locationError, setLocationError] = useState(null);
   const [success, setSuccess] = useState(null);
+  const [isClient, setIsClient] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+
+  useEffect(() => {
+    const address = localStorage.getItem("userAddress");
+    setIsClient(true);
+    if (address) {
+      setUserAddress(address);
+    } else {
+      setShowModal(true);
+    }
+  }, []);
+
   const handleLocationDetection = async () => {
     try {
       setLocationError(null);
@@ -32,7 +43,7 @@ const LocationAccess = ({ apiKey }) => {
       if (coordinates) {
         setLocation(coordinates);
         const { latitude, longitude } = coordinates;
-        if(!localStorage.getItem("userAddress")){
+        if (!localStorage.getItem("userAddress")) {
           const response = await axios.get(
             `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
           );
@@ -71,70 +82,76 @@ const LocationAccess = ({ apiKey }) => {
   };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <div>
-          <button
-            variant="link"
-            className={`w-full bg-transparent flex items-center text-[13px] border-none justify-center px-0 py-0
-             font-medium ${
-               userAddress
-                 ? "text-black  hover:bg-transparent hover:text-black"
-                 : " text-black hover:bg-transparent hover:text-gray-500"
-             }`}
-          >
-            {userAddress
-              ? userAddress.split(",").slice(-2, -1)[0].trim()
-              : "Location"}
-            <ChevronDown className="ml-2 h-4 w-4" />
-          </button>
-        </div>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>
-            {userAddress !== null ? "Your Location" : "Detect Location"}
-          </DialogTitle>
-          <DialogDescription>
-            {userAddress !== null
-              ? userAddress
-              : "Enter your location to get started."}
-          </DialogDescription>
-        </DialogHeader>
-        {userAddress === null ? (
-          <div className="grid gap-4 py-4">
-            <div className="flex flex-col gap-1">
-              <div className="flex gap-2 items-center flex-col md:flex-row">
-                <Button
-                  onClick={handleLocationDetection}
-                  className="bg-green-700 w-full md:flex-1 text-white font-normal tracking-wide hover:bg-green-700"
-                >
-                  Detect my location
-                </Button>
-                <p className=" text-black font-bold text-center">OR</p>
-                <Input
-                  id="location"
-                  defaultValue={userAddress}
-                  placeholder="Enter your location"
-                  className="col-span-3 flex-1"
-                />
-              </div>
-              {locationError ? (
-                <p className="text-red-500 text-[12px] text-left">
-                  {locationError}
-                </p>
-              ) : null}
-
-              {success ? (
-                <p className="text-green-600 text-[12px] w-full">{success}</p>
-              ) : null}
+    <>
+      {isClient && (
+        <Dialog defaultOpen={showModal}>
+          <DialogTrigger asChild>
+            <div>
+              <button
+                variant="link"
+                className={`w-full bg-transparent flex items-center text-[13px] border-none justify-center px-0 py-0
+               font-medium ${
+                 userAddress
+                   ? "text-black  hover:bg-transparent hover:text-black"
+                   : " text-black hover:bg-transparent hover:text-gray-500"
+               }`}
+              >
+                {userAddress
+                  ? userAddress.split(",").slice(-2, -1)[0].trim()
+                  : "Location"}
+                <ChevronDown className="ml-2 h-4 w-4" />
+              </button>
             </div>
-          </div>
-        ) : (
-          <></>
-        )}
-      </DialogContent>
-    </Dialog>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>
+                {userAddress !== null ? "Your Location" : "Detect Location"}
+              </DialogTitle>
+              <DialogDescription>
+                {userAddress !== null
+                  ? userAddress
+                  : "Enter your location to get started."}
+              </DialogDescription>
+            </DialogHeader>
+            {userAddress === null ? (
+              <div className="grid gap-4 py-4">
+                <div className="flex flex-col gap-1">
+                  <div className="flex gap-2 items-center flex-col md:flex-row">
+                    <Button
+                      onClick={handleLocationDetection}
+                      className="bg-green-700 w-full md:flex-1 text-white font-normal tracking-wide hover:bg-green-700"
+                    >
+                      Detect my location
+                    </Button>
+                    <p className=" text-black font-bold text-center">OR</p>
+                    <Input
+                      id="location"
+                      defaultValue={userAddress}
+                      placeholder="Enter your location"
+                      className="col-span-3 flex-1"
+                    />
+                  </div>
+                  {locationError ? (
+                    <p className="text-red-500 text-[12px] text-left">
+                      {locationError}
+                    </p>
+                  ) : null}
+
+                  {success ? (
+                    <p className="text-green-600 text-[12px] w-full">
+                      {success}
+                    </p>
+                  ) : null}
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
   );
 };
 
