fb7029020f5a1eb2957765aa9dd2753dd1c3a386
satyamx <satyamsingh748846@gmail.com>
2024-05-11 11:44:00 +0530

    search bar

--- src/components/user/store/Search.jsx
+++ src/components/user/store/Search.jsx
@@ -25,7 +25,7 @@ const SearchComponent = () => {
   }, 300);
 
   return (
-    <div className="hidden md:flex flex-1  max-w-[350px] h-[40px] items-center justify-between gap-2 p-0.5 border border-black/[0.5] rounded-full ">
+    <div className="hidden md:flex flex-1  max-w-[350px] h-[40px] items-center justify-between gap-2 p-0.5 border-2 border-black/[0.5] rounded-full ">
       <form
         onSubmit={onSearch}
         className="flex h-[100%] rounded-full px-2 hover:bg-gray-200 items-center w-[100%]"
@@ -36,12 +36,14 @@ const SearchComponent = () => {
         <Input
           type="text"
           placeholder="Search"
-          className=" h-[40px]  text-[14px] border-none outline-none bg-transparent placeholder:text-gray-700"
+          className=" h-[40px] text-black  text-[14px] border-none outline-none bg-transparent placeholder:text-gray-700"
           onChange={onSearch}
         />
       </form>
-      <div className="flex items-center gap-2 px-4 hover:rounded-full border-l border-black/[0.3] hover:border-none cursor-pointer hover:bg-gray-200 h-[100%]">
-        <p className=" text-gray-700 text-[14px]">Services</p>
+      <div className="flex items-center gap-2 px-4 hover:rounded-full  cursor-pointer hover:bg-gray-200 h-[100%]">
+        <p className=" text-gray-700 text-[14px] border-l-2 pl-2 border-black/[0.5] hover:border-none">
+          Services
+        </p>
         <ChevronDown size={20} className="text-gray-700" />
       </div>
     </div>
