d1dd7a85ab01a6fdfbe1fb414160deb07d387c4c
satyamx <satyamsingh748846@gmail.com>
2024-05-15 20:54:54 +0530

    search improved

--- src/components/user/store/Search.jsx
+++ src/components/user/store/Search.jsx
@@ -1,24 +1,16 @@
 "use client";
 import React, { useState } from "react";
-import { Handshake, Phone, Search, Store } from "lucide-react";
+import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { ChevronDown } from "lucide-react";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
@@ -27,10 +19,10 @@ const SearchComponent = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const [searchQueryParameter, setSearchQueryParameter] = useState("");
+  const [searchQueryParameter, setSearchQueryParameter] = useState("Name");
 
   const [outerSearchHoverColor, setOuterSearchHoverColor] = React.useState(
-    "bg-white border-2 border-gray-700/[0.4]"
+    "bg-white border-[2px] border-gray-600/[0.2]"
   );
   const [searchType, setSearchType] = useState("bg-white");
 
@@ -45,19 +37,29 @@ const SearchComponent = () => {
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
-  const handleSelectItemClick = (e) => {
-    setOuterSearchHoverColor("bg-white border-2 border-gray-700/[0.4]");
-    setSearchQueryParameter(e);
+  const onClickName = () => {
+    setOuterSearchHoverColor("bg-white border-[2px] border-gray-600/[0.2]");
+    setSearchQueryParameter("Name");
+  };
+
+  const onClickLocation = () => {
+    setOuterSearchHoverColor("bg-white border-[2px] border-gray-600/[0.2]");
+    setSearchQueryParameter("Location");
+  };
+
+  const onClickPhone = () => {
+    setOuterSearchHoverColor("bg-white border-[2px] border-gray-600/[0.2]");
+    setSearchQueryParameter("Phone");
   };
 
   return (
     <div
       className={`hidden md:flex flex-1  max-w-[350px] h-[40px] items-center justify-between gap-2 p-0.5  rounded-full transition duration-500 ${outerSearchHoverColor}`}
       onMouseEnter={() =>
-        setOuterSearchHoverColor("bg-gray-200 border-2 border-white ")
+        setOuterSearchHoverColor("bg-gray-200 border-[2px] border-white ")
       }
       onMouseLeave={() =>
-        setOuterSearchHoverColor("bg-white border-2 border-gray-700/[0.4]")
+        setOuterSearchHoverColor("bg-white border-[2px] border-gray-600/[0.2]")
       }
     >
       <form
@@ -67,12 +69,12 @@ const SearchComponent = () => {
         onMouseLeave={() => setSearchType("bg-white")}
       >
         <button type="submit">
-          <Search size={20} className="text-black" />
+          <Search size={20} className="text-gray-700" />
         </button>
         <Input
           type="text"
           placeholder="Search store by"
-          className=" h-[40px] text-black  text-[14px] border-none outline-none bg-transparent placeholder:text-gray-700 placeholder:font-medium"
+          className=" h-[40px] text-black  text-[14px] border-none outline-none bg-transparent"
           onChange={onSearch}
         />
       </form>
@@ -84,29 +86,43 @@ const SearchComponent = () => {
           rounded-full  cursor-pointer text-gray-700 hover:bg-white h-[100%] border-none transition duration-500 ${searchType}`}
         >
           <div className="flex items-center gap-2">
-            <p className="text-gray-700 text-[14px] font-medium">Services</p>
+            <p className="text-gray-700 text-[14px] font-medium">
+              {searchQueryParameter}
+            </p>
             <ChevronDown size={20} className="text-gray-700" />
           </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>Search by</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={handleSelectItemClick}>
-            <div className="flex items-center gap-2 text-gray-700">
-              <Store size={17} className="text-gray-700" />
-              <p>Name</p>
+          <DropdownMenuItem
+            onClick={onClickName}
+            className={searchQueryParameter === "Name" ? "bg-gray-200" : ""}
+          >
+            <div className="flex flex-col text-gray-700 cursor-pointer">
+              <p className="text-[13px]">Name </p>
+              <p className="text-[11px] text-gray-600">Find stores by name</p>
             </div>
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={handleSelectItemClick} >
-            <div className="flex items-center gap-2 text-gray-700">
-              <Handshake size={17} className="text-gray-700" />
-              <p>Services</p>
+
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={onClickPhone}
+            className={searchQueryParameter === "Phone" ? "bg-gray-200" : ""}
+          >
+            <div className="flex flex-col text-gray-700 cursor-pointer">
+              <p className="text-[13px]">Phone</p>
+              <p className="text-[11px] text-gray-600">
+                Find stores by phone number
+              </p>
             </div>
-          </DropdownMenuItem >
-          <DropdownMenuItem onClick={handleSelectItemClick}>
-            <div className="flex items-center gap-2 text-gray-700">
-              <Phone size={17} className="text-gray-700" />
-              <p>Phone</p>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={onClickLocation}
+            className={searchQueryParameter === "Location" ? "bg-gray-200" : ""}
+          >
+            <div className="flex flex-col text-gray-700 cursor-pointer">
+              <p className="text-[13px]">Location </p>
+              <p className="text-[11px] text-gray-600">Find stores by location</p>
             </div>
           </DropdownMenuItem>
         </DropdownMenuContent>
