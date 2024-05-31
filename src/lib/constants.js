8349888ef8f715e1b6e1bef44ddcd19fbd36335b
satyamx <satyamsingh748846@gmail.com>
2024-05-31 10:28:25 +0530

    clear cart after succeessfull payment

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-// export const API_DOMAIN="https://api.dokopi.com"
-export const API_DOMAIN = "http://localhost:4000";
+export const API_DOMAIN="https://api.dokopi.com"
+// export const API_DOMAIN = "http://localhost:4000";
 
 
