88f2667ce6aca21c39471e1a0d4fbe8343ae9d91
Yash Chopra <chopray567@gmail.com>
2024-06-01 14:59:33 +0530

    Store Overview Work

--- src/components/user/store/store-profile/StoreInfo.jsx
+++ src/components/user/store/store-profile/StoreInfo.jsx
@@ -1,15 +1,29 @@
 "use client";
-import React from "react";
+import { Day } from "@/lib/constants";
+import React, { useEffect, useState } from "react";
 import { TiPlus, TiStarFullOutline } from "react-icons/ti";
 
-const StoreInfo = (props) => {
+const StoreInfo = ({ storeData }) => {
+  const { storeDetails, storeCurrentStatus } = storeData;
+  const { storeOpeningHours, storeLocation, storeServices } = storeDetails
+  const [workingHours, setWorkingHours] = useState(null)
+  const [TodayDay, SetTodayDay] = useState(Day[new Date().getDay()])
+  function GetOpeningTimeStore() {
+    setWorkingHours(storeOpeningHours[TodayDay])
+  }
+  useEffect(() => {
+    if (storeOpeningHours) {
+      GetOpeningTimeStore()
+    }
+  }, [])
   return (
     <div className="my-4">
       <div className="flex flex-col-reverse md:flex-row md:items-center justify-between gap-3">
-        <h1 className="text-3xl font-semibold">Jadav Jumbo Xerox</h1>
+        <h1 className="text-3xl font-semibold">{storeDetails.storeName}</h1>
         <div className="flex items-center gap-6 text-xs md:text-base">
           <div className="flex items-center gap-2">
             <span className="flex rounded items-center gap-1 text-white font-medium bg-blue-600 px-2 py-1">
+              {/* Active Orders is not in the database  */}
               {10}
               <TiPlus />
             </span>
@@ -22,6 +36,7 @@ const StoreInfo = (props) => {
           </div>
           <div className="flex items-center gap-2">
             <span className="flex rounded items-center gap-1 text-white font-medium bg-blue-600 px-2 py-1">
+              {/* Review Orders is not in the database  */}
               {4.5} <TiStarFullOutline />
             </span>
             <span>
@@ -32,12 +47,27 @@ const StoreInfo = (props) => {
         </div>
       </div>
       <div className="text-base md:text-lg text-gray-600 flex flex-col gap-2 md:block">
-        <h3>Vadgaon budruk pune, Maharastra</h3>
+        <h3>
+          {storeLocation.storeLandmark},
+          {storeLocation.storeCity},
+          {storeLocation.storeState},
+          {storeLocation.storeCountry},
+          {storeLocation.storeZipCode}</h3>
         <h3 className="text-gray-400 text-[15px] font-normal">
-          Printing, Binding, Cutting, Filing, Scanning
+          {
+            storeServices && storeServices.map((service, index, arr) => {
+              const size = arr.length;
+              return <span key={index}>{service} {index + 1 == size ? ' ' : ','}</span>
+            }
+            )}
         </h3>
         <div className="text-sm">
-          <span className="text-green-600 ">Open Now</span> - 11am - 8pm (Today)
+          {
+            !storeCurrentStatus.type == 'offline' ? <span className="text-red-600 ">Closed</span> : (workingHours == 'Closed') ? <span className="text-red-600 ">Closed On {TodayDay}</span> : <>
+              <span className="text-green-600 ">Open Now</span> - {
+                workingHours && workingHours
+              } (Today)</>
+          }
         </div>
       </div>
     </div>
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,5 @@
-export const API_DOMAIN="https://api.dokopi.com"
-// export const API_DOMAIN = "http://localhost:4000";
+// export const API_DOMAIN="https://api.dokopi.com"
+export const API_DOMAIN = "http://localhost:4000";
 
 
+export const Day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
