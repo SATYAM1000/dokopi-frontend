c9bc87c79dd48343bbcac6a59e4b90d04ff83970
satyamx <satyamsingh748846@gmail.com>
2024-06-02 20:12:10 +0530

    all pages done

--- src/components/user/home/Hero.jsx
+++ src/components/user/home/Hero.jsx
@@ -27,13 +27,13 @@ export default function Hero() {
       </div>
       <div className="mx-auto max-w-2xl py-32 sm:py-32 lg:py-28">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 flex items-center gap-1">
+          {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 flex items-center gap-1">
             <a href="#" className="font-semibold text-blue-600">
               <span className="absolute inset-0" aria-hidden="true" />
               <FaTelegramPlane size={20} />
             </a>
             Introducing Telegram Bot Ordering!{" "}
-          </div>
+          </div> */}
         </div>
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
