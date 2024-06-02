5f157695a40569a0280d9db91f63f814e4f7e1e7
satyamx <satyamsingh748846@gmail.com>
2024-06-02 08:06:35 +0530

    loader for invoice

--- src/components/user/order-details/DownloadButton.jsx
+++ src/components/user/order-details/DownloadButton.jsx
@@ -5,10 +5,13 @@ import Link from "next/link";
 import React from "react";
 import { toast } from "sonner";
 import { API_DOMAIN } from "@/lib/constants";
+import { ClipLoader } from "react-spinners";
 
 const DownloadButton = ({ id }) => {
+  const [loading, setLoading] = React.useState(false);
   const handleDownload = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(`${API_DOMAIN}/api/v1/invoice/${id}`, {
         responseType: "blob",
       });
@@ -24,6 +27,8 @@ const DownloadButton = ({ id }) => {
     } catch (error) {
       console.log(error);
       toast.error("Failed to download invoice");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -35,7 +40,13 @@ const DownloadButton = ({ id }) => {
           className="w-full rounded-md bg-white hover:bg-white/90 px-3 py-2 text-sm font-semibold text-blue-600 shadow-sm "
         >
           Download Invoice
-          <Download className="ml-2 h-4 w-4" />
+          {loading ? (
+            <>
+              <ClipLoader color="#2563eb" size={18} className="ml-2" />
+            </>
+          ) : (
+            <Download className="ml-2 h-4 w-4" />
+          )}
         </Button>
         <Link
           href="/stores"
