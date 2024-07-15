adff8fb5dfd078260b601e2aedcd2aa75d97de6a
satyamx <satyamsingh748846@gmail.com>
2024-07-15 11:35:50 +0530

    dynamic import

--- src/components/user/cart/UpdateFileInfo.jsx
+++ src/components/user/cart/UpdateFileInfo.jsx
@@ -181,11 +181,17 @@ const UpdateFileInfo = ({
           <Input
             id="updatedCopiesCount"
             type="number"
+            name="copiesCount"
             min="1"
             defaultValue={uploadedFileInfo.copiesCount || ""}
             required={true}
             className="w-full border border-gray-200 rounded-sm h-[40px] pl-2"
-            onChange={handleInputChange}
+            onChange={(e) => {
+              setUploadedFileInfo((prevInfo) => ({
+                ...prevInfo,
+                copiesCount: e.target.value,
+              }));
+            }}
           />
         </div>
 
