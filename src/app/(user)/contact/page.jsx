50ed4f799ed294a71e139ba7002ddc2e7df08558
satyamx <satyamsingh748846@gmail.com>
2024-07-28 15:06:43 +0530

    support form done

--- src/app/(user)/contact/page.jsx
+++ src/app/(user)/contact/page.jsx
@@ -1,46 +1,20 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import Footer from "@/components/user/global/Footer";
+import SupportForm from "@/components/user/avatar/SupportForm";
 import Wrapper from "@/components/user/global/Wrapper";
-import { useCurrentUser } from "@/hooks/use-current-user";
-import { Instagram, MapPin, Phone, Store } from "lucide-react";
-import Link from "next/link";
-import React, { useState } from "react";
-import { toast } from "sonner";
+import React from "react";
 
-export default function ContactUsPage() {
-  const currentUser = useCurrentUser();
-
-  const [contactData, setContactData] = useState({
-    name: currentUser?.name || "",
-    email: currentUser?.email || "",
-    message: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setContactData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (!contactData.name || !contactData.email || !contactData.message) {
-      toast.error("All fields are required");
-      return;
-    } else {
-      toast.success("Message submitted successfully!");
-    }
-  };
+const SupportPage = () => {
   return (
-    <>
-     
-      <Footer />
-    </>
+    <Wrapper className={"w-full relative h-auto mt-3"}>
+      <div>
+        <h2 className="text-2xl font-semibold">Contact Us</h2>
+        <p className="text-gray-600 text-sm">
+          If you have any questions or concerns, please don't hesitate to get in
+          touch with our support team. We are here to help!
+        </p>
+      </div>
+      <SupportForm />
+    </Wrapper>
   );
-}
+};
+
+export default SupportPage;
