67b18ab33e7b4a4af5a4bb07c7b4ce9d190904d5
satyamx <satyamsingh748846@gmail.com>
2024-07-31 04:02:27 +0530

    fixed issue

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,62 +2,62 @@ import { fetchAllStoresId } from "@/actions/get-all-stores";
 
 export default async function sitemap() {
   const baseURL = "https://dokopi.com";
+  let allStores = [];
+
   try {
     const stores = await fetchAllStoresId();
-    let allStores = [];
     if (stores?.length > 0) {
-      allStores = stores?.map((store) => {
-        return {
-          url: `${baseURL}/stores/${store?._id}`,
-          lastModified: `${store?.updatedAt}`,
-          changeFrequency: "weekly",
-          priority: 0.5,
-        };
-      });
+      allStores = stores.map((store) => ({
+        url: `${baseURL}/stores/${store._id}`,
+        lastModified: new Date(store.updatedAt).toISOString(),
+        changeFrequency: "weekly",
+        priority: 0.5,
+      }));
     }
   } catch (error) {
     console.error(error);
   }
+
   return [
     {
       url: "https://dokopi.com",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
       url: "https://dokopi.com/stores",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "weekly",
       priority: 0.5,
     },
     {
       url: "https://dokopi.com/about",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://dokopi.com/terms-conditions",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://dokopi.com/privacy-policy",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://dokopi.com/refund-shipping-policy",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://dokopi.com/contact",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
