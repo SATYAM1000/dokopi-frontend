e31cf332bc4deb8e469d40b142c82ef430eface0
satyamx <satyamsingh748846@gmail.com>
2024-08-11 16:04:57 +0530

    changed favicon

--- src/components/user/home/Hero.jsx
+++ src/components/user/home/Hero.jsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { MoveRight } from 'lucide-react';
-
+import { MoveRight } from "lucide-react";
 
 import Link from "next/link";
 
@@ -23,15 +22,7 @@ export default function Hero() {
         />
       </div>
       <div className="mx-auto max-w-2xl py-32 sm:py-32 lg:py-28">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 flex items-center gap-1">
-            <a href="#" className="font-semibold text-blue-600">
-              <span className="absolute inset-0" aria-hidden="true" />
-              <FaTelegramPlane size={20} />
-            </a>
-            Introducing Telegram Bot Ordering!{" "}
-          </div> */}
-        </div>
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Print with ease. Anywhere. Anytime.
@@ -42,7 +33,7 @@ export default function Hero() {
             printing sector.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link href="/stores">
+            <Link prefetch={true} href="/stores">
               <Button className="px-12 py-6  rounded-full bg-black tracking-wide text-[16px] flex items-center gap-2">
                 Explore Now
                 <MoveRight size={20} />
